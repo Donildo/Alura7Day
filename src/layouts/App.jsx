7716845c76603f3c28986b232e9d7d8eeecd7c29
Donildo <donildobrito307@gmail.com>
2023-12-13 09:18:06 -0400

    alura

---
+++ src/layouts/App.jsx
@@ -0,0 +1,7 @@
+import PropTypes from "prop-types"
+
+export const App = ({ children }) => <>{children}</>
+
+App.PropTypes = {
+    children: PropTypes.node,
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import { App } from "../layouts/App"
+
+export const Home = () => {
+    <App>
+        <h1 className="text-x1 text-sky-600">Home</h1>
+    </App>
+}
